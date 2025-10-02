f4fdaf3a1a93f74a90305ead96ce3cb65dd1d7e8
Radoslav <119621781+RadoslavTsvetanov@users.noreply.github.com>
2025-10-02 14:38:39 +0300

    Create matchUnion.ts

---
+++ src/functions/matchUnion.ts
@@ -0,0 +1,59 @@
+
+type UnionToIntersection<U> =
+  (U extends any ? (x: U) => void : never) extends (x: infer I) => void
+    ? I
+    : never;
+
+
+type LastOf<U> =
+  UnionToIntersection<U extends any ? (x: U) => void : never> extends (x: infer L) => void
+    ? L
+    : never;
+
+type UnionToTuple<U, T extends any[] = []> =
+  [U] extends [never]
+    ? T
+    : UnionToTuple<Exclude<U, LastOf<U>>, [LastOf<U>, ...T]>;
+
+class Bae<Name extends string> {
+  constructor(public readonly kind: Name) { }
+}
+
+class Circle extends Bae<"circle"> {
+  constructor() {
+    super("circle")
+  }
+
+  public radius: number = 0
+}
+
+class Rectangle extends Bae<"rect"> {
+  constructor() {
+    super("rect")
+  }
+
+  public wall = 9
+}
+
+type TupleToRecord<T extends Bae<any>[]> = {
+    [P in T[number] as P['kind']]: P
+};  
+
+function match<T extends Bae<any>>(
+  v: T,
+  handler: { 
+    [K in keyof TupleToRecord<UnionToTuple<T>>]: (arg: TupleToRecord<UnionToTuple<T>>[K]) => unknown
+}
+) {
+  console.log("not implemented")
+}
+
+const a: Circle | Rectangle = null
+
+match(
+    a,
+    {
+        circle:v => v.radius,
+        rect: v => v.wall
+    }
+)
