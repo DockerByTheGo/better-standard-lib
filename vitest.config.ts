04445f15605eb96298abf1700e70be3002b26aba
radoslav-tsvetanov-themuse <radoslav.tsvetanov@themuse.com>
2026-05-06 01:00:28 +0300

    added ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vitest/config";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
+import { defineConfig } from "vitest/config";
 
 const currentDir = dirname(fileURLToPath(import.meta.url));
 
