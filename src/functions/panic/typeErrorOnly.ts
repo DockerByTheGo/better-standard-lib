b12ccb7ceabbd110eba5e4b00d423eebade0074e
radoslav-tsvetanov-themuse <radoslav.tsvetanov@themuse.com>
2026-05-28 15:13:17 +0300

    minor fix

--- src/functions/panic/typeErrorOnly.ts
+++ src/functions/panic/typeErrorOnly.ts
@@ -1,3 +1,5 @@
 import { panic } from "./default";
 
 export const panicTypeOnlyVariable = (msg?: string) => panic(msg ?? "The thing you are accessing is only supposed to provide a type and shoulddnt be used to get an actual value")
+
+export const panicTypeOnlyFunction = (msg?: string) => panic(msg ?? "The method you are calling is only supposed to provide a type and shoulddnt be used to get an actual value")
